54ff0779df3d87e5fe47e7f150bf0a7575c3c76d
Raboro <marius.woerfelreal@gmail.com>
2023-08-31 21:39:19 +0200

    fixed validate script

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -10,13 +10,13 @@ ends_not_with_period() {
 }
 
 package_name=$(node -pe "require('./package.json').name")
-manifest_name=$(node -pe "require('./manifest.json').name")
+manifest_id=$(node -pe "require('./manifest.json').id")
 package_description=$(node -pe "require('./package.json').description")
 manifest_description=$(node -pe "require('./manifest.json').description")
 
 echo -e "\nvalidate that package.json and manifest.json names are equal"
 
-if [ "$package_name" != "$manifest_name" ]; then
+if [ "$package_name" != "$manifest_id" ]; then
     echo "Name mismatch between package.json and manifest.json. Exiting."
     exit 1
 fi
