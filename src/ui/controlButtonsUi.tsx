d2c791023802c1feb3dc12c1e74f6e5de1bf6c34
Raboro <marius.woerfelreal@gmail.com>
2023-08-31 23:16:31 +0200

    added button states and switch between them

--- src/ui/controlButtonsUi.tsx
+++ src/ui/controlButtonsUi.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 export default function ControlButtonsUi() {
+    const [startDisplay, setStartDisplay] = useState(true);
+    const [cancelDisplay, setCancelDisplay] = useState(false);
+    const [pauseDisplay, setPauseDisplay] = useState(false);
+
+    function switchDisplay() {
+        setStartDisplay(prevDisplay => !prevDisplay);
+        setCancelDisplay(prevDisplay => !prevDisplay);
+        setPauseDisplay(prevDisplay => !prevDisplay); 
+    }
+
+    function start() {
+        switchDisplay();
+        // TODO trigger ClockUi here
+    }
+
+    function cancel() {
+        switchDisplay();
+        // TODO trigger ClockUi here
+    }
+
+    function pause() {
+        // TODO trigger ClockUi here
+    }
+
     return <div className="controlButtonsContainer">
-        <button>Start</button>
-        <button>Cancel</button>
-        <button>Pause</button>
+        {startDisplay && <button onClick={start}>Start</button>}
+        {cancelDisplay && <button onClick={cancel}>Cancel</button>}
+        {pauseDisplay && <button onClick={pause}>Pause</button>}
     </div>;
 }
