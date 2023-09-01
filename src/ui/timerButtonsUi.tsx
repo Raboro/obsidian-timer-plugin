f623a3077b46e621d8930c6cfdfd4e1a26b11e10
Raboro <marius.woerfelreal@gmail.com>
2023-09-01 22:22:39 +0200

    added timerButtons with styles to change timer

---
+++ src/ui/timerButtonsUi.tsx
@@ -0,0 +1,12 @@
+export default function TimerButtonsUi() {
+    return <div className="timerButtonsContainer">
+        <button>-1h</button>
+        <button>-10m</button>
+        <button>-1m</button>
+        <button>-1s</button>
+        <button>+1s</button>
+        <button>+1m</button>
+        <button>+10m</button>
+        <button>+1h</button>
+    </div>;
+}
--- src/ui/timerUi.tsx
+++ src/ui/timerUi.tsx
@@ -1,9 +1,11 @@
 import ClockUi from './clockUi';
 import ControlButtonsUi from './controlButtonsUi';
+import TimerButtonsUi from './timerButtonsUi';
 
 export default function TimerUi() {
     return <>
         <ClockUi />
+        <TimerButtonsUi />
         <ControlButtonsUi />
     </>;
 }
