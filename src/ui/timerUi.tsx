6bf7e3efb53d454df78d5170d081a53bd79892ce
Raboro <marius.woerfelreal@gmail.com>
2023-09-01 21:23:27 +0200

    integrated clockUi

--- src/ui/timerUi.tsx
+++ src/ui/timerUi.tsx
@@ -1,7 +1,9 @@
+import ClockUi from './clockUi';
 import ControlButtonsUi from './controlButtonsUi';
 
 export default function TimerUi() {
     return <>
+        <ClockUi />
         <ControlButtonsUi />
     </>;
 }
