009c049c8222c76f6a5a022fc96875af3a560fb0
Raboro <marius.woerfelreal@gmail.com>
2023-09-01 21:22:56 +0200

    added text to clockTextElementUi

--- src/ui/clockTextElementUi.tsx
+++ src/ui/clockTextElementUi.tsx
@@ -1,3 +1,7 @@
-export default function ClockTextElementUi() {
-    return <></>;
+interface IClockTextElementUi {
+    text: string;
+}
+
+export default function ClockTextElementUi({text}: IClockTextElementUi) {
+    return <h3>{text}</h3>;
 }
