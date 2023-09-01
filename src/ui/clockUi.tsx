c000f33bb8b50167e822c6f0d8a85b707ff47ca7
Raboro <marius.woerfelreal@gmail.com>
2023-09-01 21:23:18 +0200

    added basic structure to clockUi with styling for textElement

--- src/ui/clockUi.tsx
+++ src/ui/clockUi.tsx
@@ -1,3 +1,23 @@
+import ClockElementSeparatorUi from "./clockElementSeparatorUi";
+import ClockElementUi from "./clockElementUi";
+import ClockTextElementUi from "./clockTextElementUi";
+
 export default function ClockUi() {
-    return <></>;
+    return <div className="clockContainer">
+        <div className="clockTextElementContainer">
+            <ClockTextElementUi text="Hours" />
+            <ClockTextElementUi text="Minutes" />
+            <ClockTextElementUi text="Seconds" />
+        </div>
+        <div className="clockElementContainer">
+            <ClockElementUi />
+            <ClockElementUi />
+            <ClockElementSeparatorUi />
+            <ClockElementUi />
+            <ClockElementUi />
+            <ClockElementSeparatorUi />
+            <ClockElementUi />
+            <ClockElementUi />
+        </div>
+    </div>;
 }
