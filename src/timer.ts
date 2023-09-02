ce235bc4d8355b063d7408afec9f4d67d1bb1866
Raboro <marius.woerfelreal@gmail.com>
2023-09-02 20:22:21 +0200

    added variables and fill constructor

--- src/timer.ts
+++ src/timer.ts
@@ -1,6 +1,16 @@
 export default class Timer {
+    private readonly HOUR_MAX: string = '99';
+    private readonly MINUTES_MAX: string = '60';
+    private readonly SECONDS_MAX: string = '60';
+
+    private hours: string;
+    private minuets: string;
+    private seconds: string;
 
     constructor(timer?: Timer) {
+        this.hours = timer?.hours ?? '00';
+        this.minuets = timer?.minuets ?? '00';
+        this.seconds = timer?.seconds ?? '00';
     }
 
     updateTimer(update: string) {
