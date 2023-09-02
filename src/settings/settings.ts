fbff15ccfad5f206f36ef48ebc122e6f02c9e052
Raboro <marius.woerfelreal@gmail.com>
2023-09-02 15:49:14 +0200

    simplified getValue & changeTimeButtonSetting & formatted

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -2,16 +2,18 @@ import { App, PluginSettingTab, Setting } from 'obsidian';
 import TimerPlugin from 'src/main';
 
 export class TimerButtonsSettings {
+    [key: string]: string
+
     first: string;
     second: string;
     third: string;
     fourth: string;
 
     constructor(first: string, second: string, third: string, fourth: string) {
-            this.first = first;
-            this.second = second;
-            this.third = third;
-            this.fourth = fourth;
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
     }
 }
 
@@ -21,7 +23,7 @@ export interface TimerSettings {
 
 export const DEFAULT_SETTINGS: TimerSettings = {
     timerButtonsSettings: new TimerButtonsSettings('1s', '1m', '10m', '1h')
-}; 
+};
 
 export class TimerSettingsTab extends PluginSettingTab {
     private plugin: TimerPlugin;
@@ -29,8 +31,9 @@ export class TimerSettingsTab extends PluginSettingTab {
     constructor(app: App, plugin: TimerPlugin) {
         super(app, plugin);
         this.plugin = plugin;
+        this.changeTimerButtonSetting = this.changeTimerButtonSetting.bind(this);
     }
-    
+
     display() {
         this.containerEl.empty();
         this.timerButtonsSettings();
@@ -39,48 +42,27 @@ export class TimerSettingsTab extends PluginSettingTab {
     private timerButtonsSettings() {
         for (let i = 0; i < 4; i++) {
             this.constructTimerButtonSetting(i);
-        } 
+        }
     }
 
     private constructTimerButtonSetting(i: number) {
         return new Setting(this.containerEl)
-        .setName(i + ' position')
-        .setDesc('Select the timer jump for the ' + i + ' element')
-        .addText(text => text
-            .setPlaceholder('Enter value')
-            .setValue(this.getValue(i))
-            .onChange(async value => await this.changeTimerButtonSetting(value, i)));
+            .setName(`${i} position`)
+            .setDesc(`Select the timer jump for the ${i} element`)
+            .addText(text => text
+                .setPlaceholder('Enter value')
+                .setValue(this.getValue(i))
+                .onChange(async value => await this.changeTimerButtonSetting(value, i)));
     }
 
     private getValue(index: number): string {
-        switch (index) {
-            case 0:
-                return this.plugin.settings.timerButtonsSettings.first;
-            case 1:
-                return this.plugin.settings.timerButtonsSettings.second;
-            case 2:
-                return this.plugin.settings.timerButtonsSettings.third;
-            case 3:
-                return this.plugin.settings.timerButtonsSettings.fourth;
-        }
-        return '';
+        const settings = this.plugin.settings.timerButtonsSettings;
+        return [settings.first, settings.second, settings.third, settings.fourth][index];
     }
 
-    private changeTimerButtonSetting = async (value: string, index: number) => {
-        switch (index) {
-            case 0:
-                this.plugin.settings.timerButtonsSettings.first = value;
-                break;
-            case 1:
-                this.plugin.settings.timerButtonsSettings.second = value;
-                break;
-            case 2:
-                this.plugin.settings.timerButtonsSettings.third = value;
-                break;
-            case 3:
-                this.plugin.settings.timerButtonsSettings.fourth = value;
-        }
+    private async changeTimerButtonSetting(value: string, index: number) {
+        const properties = ['first', 'second', 'third', 'fourth'];
+        this.plugin.settings.timerButtonsSettings[properties[index]] = value;
         await this.plugin.saveSettings();
-    };
-    
+    }
 }
