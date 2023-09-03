e17a7d91f00cd9110174b12ed80c25a77649a137
Raboro <marius.woerfelreal@gmail.com>
2023-09-03 17:56:48 +0200

    added reset button

--- src/ui/controlButtonsUi.tsx
+++ src/ui/controlButtonsUi.tsx
@@ -2,17 +2,23 @@ import { useState } from 'react';
 
 export default function ControlButtonsUi() {
     const [startDisplay, setStartDisplay] = useState(true);
+    const [resetDisplay, setResetDisplay] = useState(true);
     const [cancelDisplay, setCancelDisplay] = useState(false);
     const [pauseDisplay, setPauseDisplay] = useState(false);
 
     function switchDisplay() {
         setStartDisplay(prevDisplay => !prevDisplay);
+        setResetDisplay(prevDisplay => !prevDisplay);
         setCancelDisplay(prevDisplay => !prevDisplay);
         setPauseDisplay(prevDisplay => !prevDisplay); 
     }
 
     function start() {
         switchDisplay();
+        // TODO trigger ClockUi here
+    }
+
+    function reset() {
         // TODO trigger ClockUi here
     }
 
@@ -27,6 +33,7 @@ export default function ControlButtonsUi() {
 
     return <div className="controlButtonsContainer">
         {startDisplay && <button onClick={start}>Start</button>}
+        {resetDisplay && <button onClick={reset}>Reset</button>}
         {cancelDisplay && <button onClick={cancel}>Cancel</button>}
         {pauseDisplay && <button onClick={pause}>Pause</button>}
     </div>;
