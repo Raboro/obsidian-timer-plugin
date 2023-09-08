6092ae9f9196f799f712f9a64355095d6a08c845
Raboro <marius.woerfelreal@gmail.com>
2023-09-08 14:39:19 +0200

    added AddFavoriteTimerModal

---
+++ src/modals/addFavoriteTimerModal.ts
@@ -0,0 +1,47 @@
+import { App, ButtonComponent, Modal, Setting } from "obsidian"
+
+export default class AddFavoriteTimerModal extends Modal {
+    private result: string;
+    private onSubmit: (result: string) => Promise<void>;
+
+    constructor(app: App, onSubmit: (result: string) => Promise<void>) {
+        super(app);
+        this.onSubmit = onSubmit;
+    }
+
+    onOpen(): void {
+        const { contentEl } = this;
+        contentEl.createEl('h1', { text: 'Add favorite timer:' });
+        this.addSettings(contentEl);
+    }
+
+    addSettings(contentEl: HTMLElement) {
+        new Setting(contentEl)
+            .setName(this.createFragment(contentEl))
+            .addText(text => text.onChange(value => this.result = value));
+        new Setting(contentEl).addButton(button => this.setButton(button));
+    }
+
+    private createFragment(contentEl: HTMLElement): DocumentFragment {
+        const fragment = new DocumentFragment();
+        fragment.appendChild(contentEl.createEl('h4', { text: 'Add another favorite timer in two ways: ' }));
+        fragment.appendChild(contentEl.createEl('p', { text: '1. In time notation: HH:MM:SS.' }));
+        fragment.appendChild(contentEl.createEl('p', { text: '2. With letters: 00h00m00s.' }));
+        return fragment;
+    }
+
+    setButton(button: ButtonComponent): any {
+        button
+            .setButtonText('Submit')
+            .setCta()
+            .onClick(() => {
+                this.close();
+                this.onSubmit(this.result);
+            });
+    }
+
+    onClose(): void {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
+}
