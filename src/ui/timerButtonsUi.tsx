0e79a6e3ff89c5b812476aa940afcbc198a3a9c7
Andrew Orobator <orobator.andrew@gmail.com>
2024-03-21 00:51:58 -0400

    Created stacked buttons UI

--- src/ui/timerButtonsUi.tsx
+++ src/ui/timerButtonsUi.tsx
@@ -8,9 +8,19 @@ interface ITimerButtonsUi {
 export default function TimerButtonsUi({ updateTimer }: ITimerButtonsUi) {
     const { first, second, third, fourth } = useContext(TimerButtonsSettingsContext);
     const settings = [first, second, third, fourth];
+    const stackTimerButtons = false; 
 
-    return <div className='timerButtonsContainer'>
-        {settings.reverse().map(v => <button key={`-${v}`} onClick={() => updateTimer(`-${v}`)}>-{v}</button>)}
-        {settings.reverse().map(v => <button key={`+${v}`} onClick={() => updateTimer(`+${v}`)}>+{v}</button>)}
-    </div>;
+    if (stackTimerButtons) {
+        return (
+            <div className='timerButtonsContainer' style={{ display: 'flex', flexDirection: 'column'}}>
+                <div>{settings.reverse().map(v => <button key={`+${v}`} onClick={() => updateTimer(`+${v}`)} style={{ margin: '5px', width: '50px' }}>+{v}</button>)}</div>
+                <div>{settings.map(v => <button key={`-${v}`} onClick={() => updateTimer(`-${v}`)} style={{ margin: '5px', width: '50px' }}>-{v}</button>)}</div>
+            </div>
+        );
+    } else {
+        return <div className='timerButtonsContainer'>
+            {settings.reverse().map(v => <button key={`-${v}`} onClick={() => updateTimer(`-${v}`)}>-{v}</button>)}
+            {settings.reverse().map(v => <button key={`+${v}`} onClick={() => updateTimer(`+${v}`)}>+{v}</button>)}
+        </div>;
+    }
 }
