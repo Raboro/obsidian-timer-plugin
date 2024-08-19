bb3f938532d2c51f139997a91d1bde18b87098dd
Raboro <marius.woerfelreal@gmail.com>
2024-08-20 00:32:28 +0200

    refactor(Test): :recycle: extracted duplicated expect logic into function

--- src/timer/__tests__/timer.spec.ts
+++ src/timer/__tests__/timer.spec.ts
@@ -3,10 +3,14 @@ import Timer from "../timer";
 
 test("Timer constructor without arguments should produce init timer with only zeros", () => {
 	const timer = new Timer();
-	expect(timer.toString()).toEqual("00:00:00");
+	timerShouldBe(timer, "00:00:00");
 });
 
 test("Init timer with chars", () => {
-    const timer = Timer.set("10h 10m 1s");
-	expect(timer.toString()).toEqual("10:10:01");
+	const timer = Timer.set("10h 10m 1s");
+	timerShouldBe(timer, "10:10:01");
 });
+
+function timerShouldBe(timer: Timer, actual: string): void {
+	expect(timer.toString()).toEqual(actual);
+}
