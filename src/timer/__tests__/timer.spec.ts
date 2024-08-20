6154699f961e2e38d40dc16b67a759ee4240df9b
Raboro <marius.woerfelreal@gmail.com>
2024-08-20 23:07:17 +0200

    test(Timer): :white_check_mark: added tests for timer

--- src/timer/__tests__/timer.spec.ts
+++ src/timer/__tests__/timer.spec.ts
@@ -11,6 +11,51 @@ test("Init timer with chars", () => {
 	timerShouldBe(timer, "10:10:01");
 });
 
+test("Init timer without chars", () => {
+	const timer = Timer.set("01:11:58");
+	timerShouldBe(timer, "01:11:58");
+});
+
+test("Timer constructor with splitted should produce correct timer", () => {
+	const timer = new Timer(undefined, ["10", "1", "2"]);
+	timerShouldBe(timer, "10:01:02");
+});
+
+test("Too big Timer update should produce max timer", () => {
+	const timer = new Timer();
+	timer.updateTimer("100h");
+	timerShouldBe(timer, "99:59:59");
+});
+
+test("Update Timer should produce correct update", () => {
+	const timer = new Timer();
+	timer.updateTimer("1m");
+	timerShouldBe(timer, "00:01:00");
+	timer.updateTimer("-1h");
+	timerShouldBe(timer, "00:00:00");
+	timer.updateTimer("59s");
+	timerShouldBe(timer, "00:00:59");
+	timer.updateTimer("2s");
+	timerShouldBe(timer, "00:01:01");
+	timer.updateTimer("-1s");
+	timerShouldBe(timer, "00:01:00");
+	timer.updateTimer("60s");
+	timerShouldBe(timer, "00:02:00");
+	timer.updateTimer("63m");
+	timerShouldBe(timer, "01:05:00");
+	expect(timer.isFinished()).toBeFalse();
+	timer.updateTimer("-3h");
+	expect(timer.isFinished()).toBeTrue();
+});
+
+test("Get access to Timer should be DTO", () => {
+	const timer = new Timer();
+	const dto = timer.access();
+	expect(dto.hours).toBe("00");
+	expect(dto.minutes).toBe("00");
+	expect(dto.seconds).toBe("00");
+});
+
 function timerShouldBe(timer: Timer, actual: string): void {
 	expect(timer.toString()).toEqual(actual);
 }
